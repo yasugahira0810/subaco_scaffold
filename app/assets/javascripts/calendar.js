3f00f8289014ff3d7ad73b0a29549b5b06b9934e
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-02-17 18:08:57 +0000

    Finally, insert event via ajax

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var select = function(start, end, allDay) {
+        var title = window.prompt("title");
+        var data = {event: {title: title,
+                            start: start.format(),
+                            end: end.format(), 
+                            allDay: true}};
+        $.ajax({
+            type: "POST",
+            url: "/events",
+            data: data,
+            success: function() {
+                calendar.fullCalendar('refetchEvents');
+            }
+        });
+        calendar.fullCalendar('unselect');
+    };
+
+    var calendar = $('#calendar').fullCalendar({
+        events: '/events.json',
+        selectable: true,
+        selectHelper: true,
+        ignoreTimezone: false,
+        select: select
+    });
+});
